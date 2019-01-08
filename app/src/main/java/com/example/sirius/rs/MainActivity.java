7dd85fb6d6f95162ac7a2b991ba819e739d5148b
ftelnov ftelnov@git.com <fntelnovfn@gmail.com>
2019-01-08 14:21:32 +0300

    First Commit

---
+++ app/src/main/java/com/example/sirius/rs/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.sirius.rs;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.media.MediaPlayer;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    boolean flag = false;
+    public MediaPlayer mediaPlayer;
+    public MediaPlayer mediaPlayer_1;
+    private Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        initw();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button but = (Button) findViewById(R.id.button);
+        but.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                TextView tx = (TextView) findViewById(R.id.towrite);
+                if (flag) {
+                    tx.setText("Oh");
+                    try {
+                        mediaPlayer.start();
+                    } catch (IllegalStateException e) {
+                        mediaPlayer.pause();
+                    }
+                    mediaPlayer.start();
+                } else {
+                    try {
+                        mediaPlayer_1.start();
+                    } catch (IllegalStateException e) {
+                        mediaPlayer_1.pause();
+                    }
+                    mediaPlayer_1.start();
+                    tx.setText("Yeah");
+                }
+                flag = !flag;
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Oh, senpaaaaaai:3", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+    }
+
+    public void initw() {
+        mediaPlayer = (MediaPlayer) MediaPlayer.create(this, R.raw.oh);
+        mediaPlayer_1 = (MediaPlayer) MediaPlayer.create(this, R.raw.yeah);
+    }
+}
