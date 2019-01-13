b4cc80c6d79d330f53d934a358606404431bcdf7
ftelnov ftelnov@git.com <fntelnovfn@gmail.com>
2019-01-13 22:39:13 +0300

    Полностью переработана нижняя менюшка, теперь она не тормозит. Использованы динамические фрагменты. Далее в плане: реализовать Map для оптимизации меню.

---
+++ app/src/main/java/com/example/sirius/rs/FoodFragment.java
@@ -0,0 +1,19 @@
+package com.example.sirius.rs;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class FoodFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_food,
+                container, false);
+        Button but = (Button) view.findViewById(R.id.buttontest);
+        return view;
+    }
+}
--- app/src/main/java/com/example/sirius/rs/MainActivity.java
+++ app/src/main/java/com/example/sirius/rs/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.sirius.rs;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -21,73 +25,85 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        initw();
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState);
+        //Fragments
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        final FoodFragment foodFragment = new FoodFragment();
+        final SearchFragment searchFragment = new SearchFragment();
+        final ProfileFragment profileFragment = new ProfileFragment();
+        final FoodListFragment foodlistFragment = new FoodListFragment();
+        final NotificationFragment notificationFragment = new NotificationFragment();
+
         setContentView(R.layout.activity_main);
-        final Button but = (Button) findViewById(R.id.button);
+
+        //Menu
+        final ImageButton food_but = (ImageButton) findViewById(R.id.foodbutton);
         final ImageButton search_but = (ImageButton) findViewById(R.id.searchbutton);
+        final ImageButton profile_but = (ImageButton) findViewById(R.id.profilebutton);
+        final ImageButton notif_but = (ImageButton) findViewById(R.id.notificationbutton);
+        final ImageButton listfood_but = (ImageButton) findViewById(R.id.foodlistbutton);
+
+        //transaction
+        fragmentManager.beginTransaction().replace(R.id.container, foodFragment).commit();
+        food_but.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                search_but.setImageResource(R.drawable.search_dark);
+                profile_but.setImageResource(R.drawable.profile_dark);
+                notif_but.setImageResource(R.drawable.notif_dark);
+                listfood_but.setImageResource(R.drawable.listfood_dark);
+                food_but.setImageResource(R.drawable.food_light);
+                fragmentManager.beginTransaction().replace(R.id.container, foodFragment).addToBackStack(null).commit();
+            }
+        });
         search_but.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 search_but.setImageResource(R.drawable.search_light);
+                profile_but.setImageResource(R.drawable.profile_dark);
+                notif_but.setImageResource(R.drawable.notif_dark);
+                listfood_but.setImageResource(R.drawable.listfood_dark);
+                food_but.setImageResource(R.drawable.food_dark);
+                fragmentManager.beginTransaction().replace(R.id.container, searchFragment).addToBackStack(null).commit();
             }
         });
-        final ImageButton profile_but = (ImageButton) findViewById(R.id.profilebutton);
+
         profile_but.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                search_but.setImageResource(R.drawable.search_dark);
                 profile_but.setImageResource(R.drawable.profile_light);
+                notif_but.setImageResource(R.drawable.notif_dark);
+                listfood_but.setImageResource(R.drawable.listfood_dark);
+                food_but.setImageResource(R.drawable.food_dark);
+                fragmentManager.beginTransaction().replace(R.id.container, profileFragment).addToBackStack(null).commit();
             }
         });
-        final ImageButton notif_but = (ImageButton) findViewById(R.id.notificationbutton);
+
         notif_but.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                search_but.setImageResource(R.drawable.search_dark);
+                profile_but.setImageResource(R.drawable.profile_dark);
                 notif_but.setImageResource(R.drawable.notif_light);
+                listfood_but.setImageResource(R.drawable.listfood_dark);
+                food_but.setImageResource(R.drawable.food_dark);
+                fragmentManager.beginTransaction().replace(R.id.container, notificationFragment).addToBackStack(null).commit();
             }
         });
-        final ImageButton food_but = (ImageButton) findViewById(R.id.foodbutton);
-        food_but.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                food_but.setImageResource(R.drawable.food_light);
-            }
-        });
-        final ImageButton listfood_but = (ImageButton) findViewById(R.id.foodlistbutton);
         listfood_but.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                search_but.setImageResource(R.drawable.search_dark);
+                profile_but.setImageResource(R.drawable.profile_dark);
+                notif_but.setImageResource(R.drawable.notif_dark);
                 listfood_but.setImageResource(R.drawable.listfood_light);
-            }
-        });
-        but.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                if (flag) {
-                    try {
-                        mediaPlayer.start();
-                    } catch (IllegalStateException e) {
-                        mediaPlayer.pause();
-                    }
-                    mediaPlayer.start();
-                } else {
-                    try {
-                        mediaPlayer_1.start();
-                    } catch (IllegalStateException e) {
-                        mediaPlayer_1.pause();
-                    }
-                    mediaPlayer_1.start();
-                }
-                flag = !flag;
-                Toast toast = Toast.makeText(getApplicationContext(),
-                        "Oh, senpaaaaaai:3", Toast.LENGTH_SHORT);
-                toast.show();
+                food_but.setImageResource(R.drawable.food_dark);
+                fragmentManager.beginTransaction().replace(R.id.container, foodlistFragment).addToBackStack(null).commit();
             }
         });
     }
 
-    public void initw() {
-        mediaPlayer = (MediaPlayer) MediaPlayer.create(this, R.raw.oh);
-        mediaPlayer_1 = (MediaPlayer) MediaPlayer.create(this, R.raw.yeah);
-    }
 }
+
