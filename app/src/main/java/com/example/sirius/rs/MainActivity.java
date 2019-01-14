4ce201c4dd1294e932d4b43a87b99b152a70f5cd
ftelnov <fntelnovfn@gmail.com>
2019-01-14 09:56:19 +0300

    Использовано 2 Map'а для оптимизации менюшки.

--- app/src/main/java/com/example/sirius/rs/MainActivity.java
+++ app/src/main/java/com/example/sirius/rs/MainActivity.java
@@ -16,12 +16,19 @@ import android.media.MediaPlayer;
 import android.widget.Toast;
 import android.content.pm.ActivityInfo;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
     boolean flag = false;
-    public MediaPlayer mediaPlayer;
-    public MediaPlayer mediaPlayer_1;
     boolean flagSearch = false;
     private Context context;
+    final Map<ImageButton, Integer> map = new HashMap<ImageButton, Integer>();
+    final Map<ImageButton, Boolean> visited = new HashMap<ImageButton, Boolean>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,28 +50,49 @@ public class MainActivity extends AppCompatActivity {
         final ImageButton profile_but = (ImageButton) findViewById(R.id.profilebutton);
         final ImageButton notif_but = (ImageButton) findViewById(R.id.notificationbutton);
         final ImageButton listfood_but = (ImageButton) findViewById(R.id.foodlistbutton);
+        map.put(food_but, R.drawable.food_dark);
+        map.put(search_but, R.drawable.search_dark);
+        map.put(profile_but, R.drawable.profile_dark);
+        map.put(notif_but, R.drawable.notif_dark);
+        map.put(listfood_but, R.drawable.listfood_dark);
+        visited.put(food_but, true);
+        visited.put(search_but, false);
+        visited.put(profile_but, false);
+        visited.put(notif_but, false);
+        visited.put(listfood_but, false);
+
+        final List<ImageButton> list = new LinkedList<ImageButton>();
+        list.add(food_but);
+        list.add(search_but);
+        list.add(profile_but);
+        list.add(notif_but);
+        list.add(listfood_but);
 
         //transaction
         fragmentManager.beginTransaction().replace(R.id.container, foodFragment).commit();
         food_but.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                search_but.setImageResource(R.drawable.search_dark);
-                profile_but.setImageResource(R.drawable.profile_dark);
-                notif_but.setImageResource(R.drawable.notif_dark);
-                listfood_but.setImageResource(R.drawable.listfood_dark);
+                if(visited.get(food_but)) return;
+
+                for(ImageButton imageButton: list){
+                    todark(imageButton);
+                }
                 food_but.setImageResource(R.drawable.food_light);
+                visited.put(food_but, true);
                 fragmentManager.beginTransaction().replace(R.id.container, foodFragment).addToBackStack(null).commit();
             }
         });
         search_but.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(visited.get(search_but)) return;
+
+                for(ImageButton imageButton: list){
+                    todark(imageButton);
+                }
                 search_but.setImageResource(R.drawable.search_light);
-                profile_but.setImageResource(R.drawable.profile_dark);
-                notif_but.setImageResource(R.drawable.notif_dark);
-                listfood_but.setImageResource(R.drawable.listfood_dark);
-                food_but.setImageResource(R.drawable.food_dark);
+                visited.put(search_but, true);
                 fragmentManager.beginTransaction().replace(R.id.container, searchFragment).addToBackStack(null).commit();
             }
         });
@@ -72,11 +100,12 @@ public class MainActivity extends AppCompatActivity {
         profile_but.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                search_but.setImageResource(R.drawable.search_dark);
+                if(visited.get(profile_but)) return;
+                for(ImageButton imageButton: list){
+                    todark(imageButton);
+                }
                 profile_but.setImageResource(R.drawable.profile_light);
-                notif_but.setImageResource(R.drawable.notif_dark);
-                listfood_but.setImageResource(R.drawable.listfood_dark);
-                food_but.setImageResource(R.drawable.food_dark);
+                visited.put(profile_but, true);
                 fragmentManager.beginTransaction().replace(R.id.container, profileFragment).addToBackStack(null).commit();
             }
         });
@@ -84,25 +113,34 @@ public class MainActivity extends AppCompatActivity {
         notif_but.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                search_but.setImageResource(R.drawable.search_dark);
-                profile_but.setImageResource(R.drawable.profile_dark);
+                if(visited.get(notif_but)) return;
+
+                for(ImageButton imageButton: list){
+                    todark(imageButton);
+                }
                 notif_but.setImageResource(R.drawable.notif_light);
-                listfood_but.setImageResource(R.drawable.listfood_dark);
-                food_but.setImageResource(R.drawable.food_dark);
+                visited.put(notif_but, true);
                 fragmentManager.beginTransaction().replace(R.id.container, notificationFragment).addToBackStack(null).commit();
             }
         });
         listfood_but.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                search_but.setImageResource(R.drawable.search_dark);
-                profile_but.setImageResource(R.drawable.profile_dark);
-                notif_but.setImageResource(R.drawable.notif_dark);
+                if(visited.get(listfood_but)) return;
+
+                for(ImageButton imageButton: list){
+                    todark(imageButton);
+                }
                 listfood_but.setImageResource(R.drawable.listfood_light);
-                food_but.setImageResource(R.drawable.food_dark);
+                visited.put(listfood_but, true);
                 fragmentManager.beginTransaction().replace(R.id.container, foodlistFragment).addToBackStack(null).commit();
             }
         });
+    }
+
+    private void todark(ImageButton imageButton) {
+        imageButton.setImageResource(map.get(imageButton));
+        visited.put(imageButton, false);
     }
 
 }
