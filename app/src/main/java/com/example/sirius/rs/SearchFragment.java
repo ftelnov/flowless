8e881f83af203a4eb34b7b5cdf2aeaf27b82580d
ftelnov ftelnov@git.com <fntelnovfn@gmail.com>
2019-01-16 16:58:34 +0300

    Работа над окном поиска.

--- app/src/main/java/com/example/sirius/rs/SearchFragment.java
+++ app/src/main/java/com/example/sirius/rs/SearchFragment.java
@@ -7,12 +7,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        return view;
     }
 
 }
