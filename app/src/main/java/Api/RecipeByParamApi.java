d197bf44c52ab03a6b6df48469ca6fa2fd34ac96
ftelnov ftelnov@git.com <fntelnovfn@gmail.com>
2019-01-20 16:48:12 +0300

    New Api

---
+++ app/src/main/java/Api/RecipeByParamApi.java
@@ -0,0 +1,23 @@
+package Api;
+
+import com.example.sirius.rs.GetModelRecipe;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface RecipeByParamApi {
+    @GET("/recipebyparam")
+    Call<GetModelRecipe> getData(@Query("name") String resourceName,
+                                 @Query("calories_min") int calories_min,
+                                 @Query("calories_max") int calories_max,
+                                 @Query("fats_min") int fats_min,
+                                 @Query("fats_max") int fats_max,
+                                 @Query("proteins_min") int proteins_min,
+                                 @Query("proteins_max") int proteins_max,
+                                 @Query("calrbonydrates_min") int calrbonydrates_min,
+                                 @Query("calrbonydrates_max") int calrbonydrates_max,
+                                 @Query("alergen") boolean flag);
+
+}
